9b25165983bf047e7cab0220df9721b20cce35ca
Jay Lee <dreampoetlee@gmail.com>
2022-04-21 23:41:09 -0400

    Added the User Stories and an image to the README.md file + Regenerated the four lists for the day trip + Placed a while loop inside of a function trying to loop through all the options for the trip

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# DayTripGenerator
-Generates random location, restaurant, and entertainment for a complete day trip.
+## DAY TRIP GENERATOR
+![Summer Day Trips](./images/summer_trips.jpg)
+
+### Learning Objective
+Use and practice Python fundamentals, with an emphasis on Single Responsibility.
+
+### Technologies
+Python
+
+### User Stories
+
+- As a developer, I want to make at least three commits with descriptive messages. 
+- As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainments in their own separate lists. 
+- As a user, I want a destination to be randomly selected for my day trip. 
+- As a user, I want a restaurant to be randomly selected for my day trip. 
+- As a user, I want a mode of transportation to be randomly selected for my day trip. 
+- As a user, I want a form of entertainment to be randomly selected for my day trip. 
+- As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things. 
+- As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections. 
+- As a user, I want to display my completed trip in the console. 
+- As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
--- day_trip.py
+++ day_trip.py
@@ -8,19 +8,30 @@ print(greeting_message)
 # Created destination list and randomily selected destination
 trip_destinations = ['Provincetown, MA', 'Fort Lauderdale, FL', 'San Juan, PR', 'Fire Island, NY', 'Palm Springs, CA', 'Honolul, HI']
 selected_destination = random.randrange(len(trip_destinations))
-print(f'We have selected {trip_destinations[selected_destination]} for your destination! Are you excited about going to this location?')
+
+# Created destination list and randomily selected restaurants 
+best_restaurants = ['Crown & Anchor Restaurant', 'Heritage', 'Oasis Tapas & Lounge', 'The Blue Whale', 'Hunters Palm Springs', 'Bacchus Waikiki']
+selected_restaurant = random.randrange(len(best_restaurants))
+
+# Created destination list and randomily selected mode of transportation
+best_transportations = ['Walk', 'Electric Bike', 'Bus', 'Train', 'Yacht', 'Helicopter']
+selected_transportation = random.randrange(len(best_transportations))
+
+# Created destination list and randomily selected entertainment 
+best_entertainments = ['Walking Tours', 'Segway Tours', 'Bus Tours', 'Cooking Classes', 'Amusement Parks', 'Theater']
+selected_entertainment = random.randrange(len(best_entertainments))
+
+print(f'For your Summer Day Trip excursion we have selected {trip_destinations[selected_destination]} for your destination, {best_restaurants[selected_restaurant]} for your perfect meal, {best_transportations[selected_transportation]} as the best way to travel around town, and {best_entertainments[selected_entertainment]} as a fun day activity. Are you excited about the selections that we made? ')
 
 # Function for user input
-confirm_choice = input('Please enter y/n to confirm this destination. ')
+confirm_choice = input('Please enter y/n to confirm these travel options. ')
 
-def user_choice(confirm_choice) :
-  while confirm_choice != 'y' :
-    print(f'We are sorry that option did not work for you. We have selected {trip_destinations[selected_destination]} as another option. Does this work for you?')
-
-  # else:
-  #   confirm_choice == 'y' :
-
-
+def user_choice(confirm_options):
+  while confirm_options != 'y':
+    selected_options = random.randrange([(len(trip_destinations)), (len(best_restaurants)), (len(best_transportations)), (len(best_entertainments))])
+    confirm_options = input(f'We are sorry that those options did not work for you. We have selected {selected_options} as another set of options for you. Does these options work for you? ')
+  else:
+    print(f'You have selected {trip_destinations[selected_destination]} as the destination for your trip. You will be dining at the {best_restaurants[selected_restaurant]} during the course of the day. You will travel around town via {best_transportations[selected_transportation]}, and for fun you will do {best_entertainments[selected_entertainment]}. We hope that you enjoy the trip that we have planned for you.')
 
 user_choice(confirm_choice)
 
@@ -52,24 +63,19 @@ user_choice(confirm_choice)
 # print('')
 
 # # List 2: Restaurants
-# best_restaurants = ['Crown & Anchor Restaurant', 'Heritage', 'Oasis Tapas & Lounge', 'The Blue Whale', 'Hunters Palm Springs', 'Bacchus Waikiki']
-# random_restaurants = random.randrange(len(best_restaurants))
+
 
 # print(best_restaurants[random_restaurants])
 
 # print(' ')
 
-# # List 3: Mode of Transportation
-# best_way_to_get_around = ['Walk', 'Electric Bike', 'Bus', 'Train', 'Yacht', 'Helicopter']
-# random_transportation = random.randrange(len(best_way_to_get_around))
+
 
 # print(best_way_to_get_around[random_transportation])
 
 # print('')
 
-# # List 4: Entertainment
-# things_to_do = ['Walking Tours', 'Segway Tours', 'Bus Tours', 'Cooking Classes', 'Amusement Parks', 'Theater']
-# random_entertainment = random.randrange(len(things_to_do))
+
 
 # print(things_to_do[random_entertainment])
 
